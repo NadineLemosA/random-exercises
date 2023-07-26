250bbadf082fae8ed592437f148fd96eefd818d5
Nadine de Lemos Aquino <nadinelemos2@gmail.com>
2023-07-26 14:31:41 -0300

    WIP funções criadas

--- practicingClasses/queueWithPeople/QueueMenu.java
+++ practicingClasses/queueWithPeople/QueueMenu.java
@@ -21,7 +21,68 @@ public class QueueMenu {
 	}
 
 	Scanner input = new Scanner(System.in);
-	void verificaEEditaAFila() {
+
+	void checkChosenOption() {
+		int userResponse;
+
+		do {
+			writeMenuOnScreen();
+			userResponse = input.nextInt();
+
+			switch (userResponse) {
+			case 1:
+				System.out.println("Digite o nome da pessoa:");
+				Person newPerson = new Person(input.next());
+				listOfPeopleCreated.add(newPerson);
+				break;
+			case 2:
+				showListOfCreatedPeople();
+				checkAndAddQueue();
+				break;
+			case 3:
+				lineOfPeople.callNextInLine();
+				break;
+			case 4:
+				lineOfPeople.showTheQueue();
+				checkAndRemoveFromQueue();
+				break;
+			case 5:
+				lineOfPeople.showTheQueue();
+				break;
+			}
+		} while (userResponse != 6);
+
+		System.out.println("SISTEMA ENCERRADO!");
+
+		input.close();
+	}
+	
+	void showListOfCreatedPeople() {
+		for (Person p : listOfPeopleCreated) {
+			System.out.print(p.id + " ");
+			System.out.println(p.name);
+		}
+	}
+	
+	void checkAndAddQueue() {
+		System.out.println("Digite o nome da pessoa que quer adicionar a fila:");
+		String informedPerson = input.next();
+		boolean personWasFound = false;
+		
+		for (int i = 0; i < listOfPeopleCreated.size(); i++) {
+			Person personFound = listOfPeopleCreated.get(i);
+			
+			if (informedPerson.equalsIgnoreCase(personFound.name)) {
+				personWasFound = true;
+				lineOfPeople.addPeopleToQueue(personFound);
+			}
+		}
+		if (!personWasFound) {
+			System.out.println("Está pessoa ainda não foi criada.");
+		}
+	}
+
+	void checkAndRemoveFromQueue() {
 		System.out.println("Digite o nome da pessoa que quer retirar da fila:");
 		String informedPerson = input.next();
 		boolean personWasFound = false;
@@ -35,78 +96,7 @@ public class QueueMenu {
 			}
 		}
 		if (!personWasFound) {
-			System.out.println("Está pessoa ainda não foi criada ou não está na fila.");
+			System.out.println("Está pessoa não está na fila.");
 		}
-	}
-	
-	void checkChosenOption() {
-		int userResponse;
-
-		do {
-			writeMenuOnScreen();
-
-			userResponse = input.nextInt();
-
-			switch (userResponse) {
-			case 1:
-				System.out.println("Digite o nome da pessoa:");
-				Person newPerson = new Person(input.next());
-				listOfPeopleCreated.add(newPerson);
-				break;
-			case 2:
-				for (Person p : listOfPeopleCreated) {
-					System.out.print(p.id + " ");
-					System.out.println(p.name);
-				}
-
-				System.out.println("Digite o nome de quem quer adicionar:");
-				String informedPerson = input.next();
-				boolean personAddedToQueue = false;
-
-				for (int i = 0; i < listOfPeopleCreated.size(); i++) {
-					Person personFound = listOfPeopleCreated.get(i);
-
-					if (informedPerson.equalsIgnoreCase(personFound.name)) {
-						personAddedToQueue = true;
-						lineOfPeople.addPeopleToQueue(personFound);
-					}
-				}
-
-				if (!personAddedToQueue) {
-					System.out.println("Está pessoa ainda não foi criada.");
-				}
-				break;
-			case 3:
-				lineOfPeople.callNextInLine();
-				break;
-			case 4:
-				lineOfPeople.showTheQueue();
-
-				System.out.println("Digite o nome da pessoa que quer retirar da fila:");
-				String informedPerson2 = input.next();
-				boolean personRemovedFromQueue = false;
-				
-				for (int i = 0; i < listOfPeopleCreated.size(); i++) {
-					Person personFound = listOfPeopleCreated.get(i);
-
-					if (informedPerson2.equalsIgnoreCase(personFound.name)) {
-						personRemovedFromQueue = true;
-						lineOfPeople.removePersonOfLine(personFound);
-					}
-				}
-
-				if (!personRemovedFromQueue) {
-					System.out.println("Está pessoa não está na fila.");
-				}
-				break;
-			case 5:
-				lineOfPeople.showTheQueue();
-				break;
-			}
-		} while (userResponse != 6);
-
-		System.out.println("SISTEMA ENCERRADO!");
-
-		input.close();
 	}
 }
